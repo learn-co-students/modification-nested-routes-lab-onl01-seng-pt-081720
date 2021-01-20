f79eced26cfbdc020b64f798520ed0395c1d2cce
Jeremy Wood <js.wood206@gmail.com>
2021-01-19 18:21:17 -0800

    Done.

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,3 +1,24 @@
 module SongsHelper
 
+    def artist_select(song)
+        if params[:artist_id]
+            song.artist_name
+        else
+            select_tag "song[artist_name]", options_from_collection_for_select(Artist.all, :name, :name), id: "song_artist_id"
+        end
+    end
+
+    def edit_song_link(song)
+        if song.artist
+            link_to "Edit Song", edit_artist_song_path(song.artist, song)
+        else
+            link_to "Edit Song", edit_song_path(song)
+        end
+    end
+
+    def current_artist(song)
+        if params[:artist_id]
+            hidden_field_tag "song[artist_name]", song.artist_name
+        end
+    end
 end
