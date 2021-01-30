7de30d615359430cd35d9fd5639dbdaeb46e0309
Angelica203 <brantleyhomebuyer@gmail.com>
2021-01-30 17:51:37 -0500

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -25,12 +25,15 @@ class SongsController < ApplicationController
   end
 
   def new
-    @song = Song.new
+    if params[:artist_id] && !Artist.exists?(params[:artist_id])
+      redirect_to artists_path, alert: "Artist not found."
+    else
+    @song = Song.new(artist_id: params[:artist_id])
+    end
   end
 
   def create
-    @song = Song.new(song_params)
-
+    @song = Song.new(song_params[:artist_id])
     if @song.save
       redirect_to @song
     else
